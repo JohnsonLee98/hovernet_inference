d03deaf39894a01fcb2b5090c020a305234b380f
Junsen Li <33750446+JohnsonLee98@users.noreply.github.com>
2020-09-17 22:45:52 +0800

    1

---
+++ data_process.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 17 19:37:39 2020
+
+@author: lenovo
+"""
+import numpy as np
+import  pandas as pd
+import os
+
+clinical = '../clinical.cart.2020-09-17.json'
+clinical_data = pd.read_json(clinical)
+biosp = '../biospecimen.cart.2020-09-17.json'
+bio_data = pd.read_json(biosp)
+meta = '../metadata.cart.2020-09-17.json'
+meta_data = pd.read_json(meta)
+
+data_dir = '../data/'
+file_list = os.listdir(data_dir)
+
+
+
+
